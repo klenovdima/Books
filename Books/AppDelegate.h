8eff2069dab22cb7652282421d107845290ce004
Dmitry Klenov <Dmitry@dmitrys-imac.local>
2016-09-19 22:08:44 +0300

    Initial Commit

---
+++ Books/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Books
+//
+//  Created by Dmitry Klenov on 19.09.16.
+//  Copyright © 2016 Klenov Dmitry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Books/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Books
+//
+//  Created by Dmitry Klenov on 19.09.16.
+//  Copyright © 2016 Klenov Dmitry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
